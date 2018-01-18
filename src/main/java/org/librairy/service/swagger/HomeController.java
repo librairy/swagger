ba474f3b0091df5948d1eae83b6d7a9c01025aaa
Carlos Badenes <cbadenes@gmail.com>
2018-01-18 13:33:23 +0100

    handled port and context by environment variables

--- src/main/java/org/librairy/service/swagger/HomeController.java
+++ src/main/java/org/librairy/service/swagger/HomeController.java
@@ -1,6 +1,8 @@
 package org.librairy.service.swagger;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/")
-    public String index() {
-        return "redirect:/api.html";
+    public String index(@RequestHeader HttpHeaders headers) {
+        headers.entrySet().stream().forEach( entry -> System.out.println("Header: " + entry.getKey() +" => " + entry.getValue()));
+
+        String host = "http://"+headers.getFirst("host");
+        String path = host.endsWith("/")? host + "api.html" : host+"/api.html";
+        //String action = "redirect:"+path;
+        String action = "redirect:/api.html";
+        System.out.println(action);
+        return action;
     }
 }
