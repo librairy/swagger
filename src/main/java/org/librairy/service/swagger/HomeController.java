fc236c3a00930d6cb58e8f54705cdd451710c893
Carlos Badenes <cbadenes@gmail.com>
2018-01-23 16:34:43 +0100

    comments removed

--- src/main/java/org/librairy/service/swagger/HomeController.java
+++ src/main/java/org/librairy/service/swagger/HomeController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping(value = "/")
     public String index(@RequestHeader HttpHeaders headers) {
-        headers.entrySet().stream().forEach( entry -> System.out.println("Header: " + entry.getKey() +" => " + entry.getValue()));
+//        headers.entrySet().stream().forEach( entry -> System.out.println("Header: " + entry.getKey() +" => " + entry.getValue()));
 
         String host = "http://"+headers.getFirst("host");
         String path = host.endsWith("/")? host + "api.html" : host+"/api.html";
         //String action = "redirect:"+path;
         String action = "redirect:/api.html";
-        System.out.println(action);
+//        System.out.println(action);
         return action;
     }
 }
