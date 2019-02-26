a00f73114617837a50c16033a7f76e1b9bbe4113
Carlos Badenes <cbadenes@gmail.com>
2019-02-26 22:45:33 +0100

    update springfox to handle api model property extended plugin

---
+++ src/main/java/org/librairy/service/ApiModelPropertyExtended.java
@@ -0,0 +1,17 @@
+package org.librairy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.lang.annotation.*;
+
+/**
+ * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Inherited
+@Documented
+public @interface ApiModelPropertyExtended {
+    String defaultValue() default "";
+}
---
+++ src/main/java/org/librairy/service/ApiModelPropertyExtendedPlugin.java
@@ -0,0 +1,52 @@
+package org.librairy.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.google.common.base.Optional;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.schema.ModelPropertyBuilderPlugin;
+import springfox.documentation.spi.schema.contexts.ModelPropertyContext;
+import springfox.documentation.swagger.common.SwaggerPluginSupport;
+
+/**
+ * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
+ */
+
+@Component
+@Order(SwaggerPluginSupport.SWAGGER_PLUGIN_ORDER + 1000)
+public class ApiModelPropertyExtendedPlugin implements ModelPropertyBuilderPlugin {
+
+    @Override
+    public void apply(ModelPropertyContext context) {
+
+        Optional<BeanPropertyDefinition> optionalDefinition = context.getBeanPropertyDefinition();
+
+        if (optionalDefinition.isPresent()) {
+
+            BeanPropertyDefinition definition = optionalDefinition.get();
+            ApiModelPropertyExtended annotation = null;
+
+            if (definition.hasGetter()) {
+                annotation = definition.getGetter().getAnnotation(ApiModelPropertyExtended.class);
+            }
+
+            if (annotation == null) {
+                annotation = definition.getField().getAnnotation(ApiModelPropertyExtended.class);
+            }
+
+            if (annotation != null) {
+                context.getBuilder()
+                        .defaultValue(annotation.defaultValue())
+                        .build();
+            }
+        }
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return true;
+    }
+}
